7ccb9dd26e8491e25bd92ef3f942c65857139e2b
Matthieu Muffato <mm49@sanger.ac.uk>
2022-04-05 19:35:19 +0100

    Message update
    cf https://github.com/singularityhub/singularity-hpc/pull/502#issuecomment-1088766543

--- shpc/main/modules/templates/includes/default_version.lua
+++ shpc/main/modules/templates/includes/default_version.lua
@@ -1,5 +1,5 @@
 {% if not settings.default_version %}if (mode() == "load") then
   if ( myModuleUsrName() ~= myModuleFullName() and myModuleUsrName() ~= string.gsub(myModuleFullName(),"/module$","") ) then
-    LmodError("You must specify module <name>/<version>.")
+    LmodError("Default modules are disabled by your systems administrator. You must specify `module load <name>/<version>`.")
   end
 end{% endif %}
