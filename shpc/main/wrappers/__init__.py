2458aebea110fd5aa53ce6769ddc7bf87fa3a6f1
Matthieu Muffato <mm49@sanger.ac.uk>
2022-04-30 12:36:58 +0100

    Defines the constructor arguments just once

--- shpc/main/wrappers/__init__.py
+++ shpc/main/wrappers/__init__.py
@@ -22,17 +22,20 @@ def generate(image, container, config, **kwargs):
     settings = container.settings
     aliases = kwargs.get("aliases", [])
 
+    constructor_kwargs = {
+        "settings": settings,
+        "config": config,
+        "container": container,
+        "image": image,
+        **kwargs,
+    }
+
     # Global wrapper scripts are generated via aliases
     for container_tech in ["docker", "podman", "singularity"]:
         template_name = settings.wrapper_scripts.get(container_tech)
         if template_name and aliases and container_tech == container.command:
             wrapper = get_wrapper_script(template_name)(
-                settings=settings,
-                wrapper_template=template_name,
-                config=config,
-                container=container,
-                image=image,
-                **kwargs
+                wrapper_template=template_name, **constructor_kwargs
             )
             generated += wrapper.generate_aliases()
 
@@ -49,12 +52,7 @@ def generate(image, container, config, **kwargs):
             continue
         for alias, template_name in listing.items():
             wrapper = get_wrapper_script(template_name)(
-                settings=settings,
-                wrapper_template=template_name,
-                config=config,
-                container=container,
-                image=image,
-                **kwargs
+                wrapper_template=template_name, **constructor_kwargs
             )
             generated += wrapper.generate(alias)
 
