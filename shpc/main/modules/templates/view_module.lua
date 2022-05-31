1cddfb04600b862e134ef87170ce0a7005af2ecd
vsoch <vsoch@users.noreply.github.com>
2022-05-31 15:45:15 -0600

    updating view recipes
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- shpc/main/modules/templates/view_module.lua
+++ shpc/main/modules/templates/view_module.lua
@@ -5,3 +5,11 @@
 {% for module in system_modules %}
 module load("{{ module }}"){% endfor %}
 
+local view_dir = string.gsub(myFileName():match("(.*[/])") or ".","/%a*/$","")
+local view_name = string.gsub(view_dir,".*/","")
+local view_module = '.view_' .. view_name
+local view_modulefile = view_dir .. '/' .. view_module .. '.lua'
+
+if isFile(view_modulefile) then
+  load(view_module)
+end
