7b49fa697202c034f44d622972f58a4a36b1e248
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2022-11-26 06:27:23 -0700

    adding shpc hosted action to update from listing and cache (#616)
    * adding shpc hosted action to update from listing and cache
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

---
+++ actions/cache-update/scripts/update-from-cache.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+# This script uses count data from https://github.com/singularityhub/shpc-registry-cache
+# to:
+
+# - Subsetting to those in a container, including counts
+# - Rank ordering from least to greatest (lower frequency is a more unique command)
+# - Including any counts with a frequency < 10
+# - Above that, including the next N, but don't go over max_count (1000)
+# - Use these to generate a new container.yaml for the file (if it does not exist yet)
+
+# It requires a local clone of shpc-registry-cache with a counts file available,
+# along with a json file with commands found there.
+# Usage:
+# python update.py --cache ../shpc-registry-cache --registry $PWD
+
+import argparse
+import os
+import sys
+
+from container_discovery.cache import iter_new_cache
+
+from shpc.main import get_client
+
+here = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, here)
+
+from helpers import add_container  # noqa
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="SHPC Container Adder",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument("--cache", help="Path to cache with counts.json.")
+    parser.add_argument(
+        "--registry", help="Path to registry root.", default=os.getcwd()
+    )
+    parser.add_argument(
+        "--url", help="URL optionally with format string %s for entry_name"
+    )
+    parser.add_argument("--maintainer", help="Author", default="@vsoch")
+    parser.add_argument(
+        "--min-count-inclusion",
+        help="Include all executables under this count",
+        default=10,
+        type=int,
+        dest="min_count",
+    )
+    parser.add_argument(
+        "--max-count-inclusion",
+        help="Do not include counts over this value",
+        default=1000,
+        type=int,
+        dest="max_count",
+    )
+    parser.add_argument(
+        "--additional-count-inclusion",
+        help="Author",
+        default=25,
+        type=int,
+        dest="add_count",
+    )
+    return parser
+
+
+def main():
+
+    parser = get_parser()
+
+    # If an error occurs while parsing the arguments, the interpreter will exit with value 2
+    args, extra = parser.parse_known_args()
+
+    # Show args to the user
+    print("            maintainer: %s" % args.maintainer)
+    print("              registry: %s" % args.registry)
+    print("                 cache: %s" % args.cache)
+
+    cli = get_client()
+    cli.settings.registry = [args.registry]
+    cli.reload_registry()
+
+    for path in args.registry, args.cache:
+        if not os.path.exists(path):
+            sys.exit(f"{path} does not exist")
+
+    for entry in iter_new_cache(args.cache, registry=args.registry):
+        aliases = entry.filter_aliases(
+            add_count=args.add_count, min_count=args.min_count, max_count=args.max_count
+        )
+
+        # Now add the container, and use the tag
+        container = f"{entry.image}:{entry.tag}"
+        entry_name = entry.image.split(os.sep)[-1]
+
+        # First derive and save the aliases - we can filter based
+        try:
+            add_container(
+                cli,
+                container,
+                args.maintainer,
+                entry_name,
+                aliases=aliases,
+                url=args.url,
+            )
+        except Exception as e:
+            print(f"Issue adding container {container}: {e}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ actions/cache-update/scripts/update-from-listing.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+# This script updates the registry from a listing. An existing list
+# of containers is required to not retrieve tags for uris that already exist
+
+import argparse
+import os
+import sys
+
+from container_discovery.listing import iter_tags
+
+import shpc.utils
+from shpc.main import get_client
+
+here = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, here)
+from helpers import add_container  # noqa
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description="SHPC Container Adder",
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+    parser.add_argument("--listing", help="Path to cache with counts.json.")
+    parser.add_argument(
+        "--namespace", help="Namespace to add to containers in listing."
+    )
+    parser.add_argument(
+        "--registry", help="Path to registry root.", default=os.getcwd()
+    )
+    parser.add_argument(
+        "--url", help="URL optionally with format string %s for entry_name"
+    )
+    parser.add_argument("--maintainer", help="Author", default="@vsoch")
+    return parser
+
+
+def main():
+
+    parser = get_parser()
+
+    # If an error occurs while parsing the arguments, the interpreter will exit with value 2
+    args, extra = parser.parse_known_args()
+
+    # Show args to the user
+    print("  maintainer: %s" % args.maintainer)
+    print("    registry: %s" % args.registry)
+    print("   namespace: %s" % args.namespace)
+    print("     listing: %s" % args.listing)
+
+    cli = get_client()
+    cli.settings.registry = [args.registry]
+    cli.reload_registry()
+
+    for path in args.registry, args.listing:
+        if not os.path.exists(path):
+            sys.exit(f"{path} does not exist")
+
+    # Read into listing of containers
+    containers = [
+        x.strip() for x in shpc.utils.read_file(args.listing).split("\n") if x.strip()
+    ]
+
+    # If a namespace is provided, add to listing
+    if args.namespace:
+        containers = ["%s/%s" % (args.namespace, x) for x in containers]
+
+    # Listing of existing containers
+    existing = list(cli.registry.iter_registry())
+
+    # Do a diff between lists, get tag for container if not added
+    # This checks the registry and ensures the path doesn't already exist
+    for container in iter_tags(containers, existing, args.registry):
+
+        # We assume we cannot get aliases here
+        try:
+            add_container(
+                cli,
+                container,
+                args.maintainer,
+                container,
+                aliases=[],
+                url=args.url,
+            )
+        except Exception as e:
+            print(f"Issue adding container {container}: {e}")
+
+
+if __name__ == "__main__":
+    main()
