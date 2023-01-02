8c7947163c40892dc0c9c917364885be1a303d79
vsoch <vsoch@users.noreply.github.com>
2023-01-02 14:13:35 -0700

    update license headers for 2023
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- shpc/client/uninstall.py
+++ shpc/client/uninstall.py
@@ -1,5 +1,5 @@
 __author__ = "Vanessa Sochat"
-__copyright__ = "Copyright 2021-2022, Vanessa Sochat"
+__copyright__ = "Copyright 2021-2023, Vanessa Sochat"
 __license__ = "MPL 2.0"
 
 import shpc.utils
