4c1cd80f6e4fd98944c8cc73cb72b05aef3d694e
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2023-04-27 13:33:04 -0600

    fix: podman inspect typo (#643)
    The podman inspect script (generated as a wrapper
    script) accidentally has singularity hard coded instead
    of the container.command template variable. This will
    fix that.
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>
    Co-authored-by: vsoch <vsoch@users.noreply.github.com>

--- shpc/main/wrappers/templates/docker/inspect.sh
+++ shpc/main/wrappers/templates/docker/inspect.sh
@@ -1,3 +1,3 @@
 {% extends "bases/shell-script-base.sh" %}
 
-{% block content %}singularity ${PODMAN_OPTS} inspect ${PODMAN_COMMAND_OPTS}  {{ image }}{% endblock %}
+{% block content %}{{ container.command }} ${PODMAN_OPTS} inspect ${PODMAN_COMMAND_OPTS}  {{ image }}{% endblock %}
