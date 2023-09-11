91b332d9f5a92b77e547bf732401f135b098be4d
vsoch <vsoch@users.noreply.github.com>
2023-09-10 21:10:00 -0600

    use wrapperDir instead of wrapper_bin for wrapper templates
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- shpc/main/wrappers/templates/bases/shell-script-base.sh
+++ shpc/main/wrappers/templates/bases/shell-script-base.sh
@@ -1,7 +1,7 @@
 #!{{ settings.wrapper_shell }}
 
 {% if '/csh' in settings.wrapper_shell %}set {% endif %}script=`realpath $0`
-{% if '/csh' in settings.wrapper_shell %}set {% endif %}wrapper_bin={% if settings.wrapper_base %}"{{ wrapper_dir }}"{% else %}`dirname $script`{% endif %}
-{% if '/csh' in settings.wrapper_shell %}set {% endif %}moduleDir=`dirname $wrapper_bin`
+{% if '/csh' in settings.wrapper_shell %}set {% endif %}wrapperDir={% if settings.wrapper_base %}"{{ wrapper_dir }}"{% else %}`dirname $script`{% endif %}
+{% if '/csh' in settings.wrapper_shell %}set {% endif %}moduleDir=`dirname $wrapperDir`
 
 {% block content %}{% endblock %}
