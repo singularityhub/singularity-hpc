4bf959f23bb8a6aa118445b05623206eb8ce69c2
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2025-01-01 18:47:19 -0700

    bug: fix distutils LooseVersion import (#680)
    * bug: fix distutils LooseVersion import
    * ci: ubuntu 24.04 does not allow unshare
    
    Problem: distutils was removed python 3.12
    Solution: import from packaging
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- shpc/tests/test_sync.py
+++ shpc/tests/test_sync.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python
 
-# Copyright (C) 2021-2023 Vanessa Sochat.
-
-# This Source Code Form is subject to the terms of the
-# Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
-# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 import os
 
 import pytest
